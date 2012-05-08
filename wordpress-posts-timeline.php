ff765402916e04182d2ce0756b0c05d9a62f127e
Wylie Hobbs <hobbsh@wit.edu>
2012-05-08 15:18:40 -0400

    multiple changes
    add featured image support, add function to only load style where short code is used

--- wordpress-posts-timeline.php
+++ wordpress-posts-timeline.php
@@ -1,15 +1,31 @@
 <?php
 /*
 	Plugin Name: WordPress Posts Timeline
-	Plugin URI: http://hackbits.com/plugins/wp-posts-timeline
+	Plugin URI: http://wordpress.org/extend/plugins/wordpress-posts-timeline
 	Description: Outputs WordPress posts in a vertical timeline
 	Author: Wylie Hobbs
 	Version: 0.2
-	Author URI: http:/hackbits.com
+	Author URI: http:/hackbits.com/demos/wordpress-posts-timeline
 	Text Domain: wordpress-posts-timeline
 	Domain Path: /lang
  */
  
+/*  Copyright YEAR  PLUGIN_AUTHOR_NAME  (email : wylie@hackbits.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 //example usage: [wp-timeline cat="5" date='F j, Y' show="15"] - show 15 posts in category-ID 5 with date format May 1, 2012
 function timeline_shortcode($atts){
 	$args = shortcode_atts( array(
@@ -17,7 +33,8 @@ function timeline_shortcode($atts){
       'type' => 'post',
       'show' => 5,
       'date' => 'Y',
-      'length' => 100
+      'length' => 100,
+      'images' => 'no'
      ), $atts );
      
      return display_timeline($args);
@@ -25,6 +42,7 @@ function timeline_shortcode($atts){
  
 add_shortcode('wp-timeline', 'timeline_shortcode');
 
+//display the timeline
 function display_timeline($args){
 
 	$post_args = array(
@@ -46,6 +64,11 @@ function display_timeline($args){
 	            echo '<h3 class="timeline-date">';
 				echo get_the_time($args['date'], $post->ID);
 				echo '</h3>';
+				if ( $args['images'] == 'yes' ){
+					if ( featured_image() == true && has_post_thumbnail( $post->ID ) ){
+						echo '<span class="timeline-image">' . get_the_post_thumbnail( $post->ID, 'timeline-thumb' ) . '</span>';
+					}
+				}
 				echo timeline_text($args['length']);
 				echo '</div></li>';
 
@@ -56,6 +79,7 @@ function display_timeline($args){
 		wp_reset_postdata();
 }
 
+//trim text function
 function timeline_text($charlength){
 	
 	$raw_text = get_the_content('', true, '');
@@ -69,16 +93,58 @@ function timeline_text($charlength){
 	}
 }
 
+//is shortcode active on page? if so, add styles to header
+function has_timeline_shortcode( $posts ) {
 
-function timeline_scripts() 
-{
+        if ( empty($posts) )
+            return $posts;
 
-	wp_register_style($handle = 'timeline', $src = plugins_url('css/timeline.css', __FILE__), $deps = array(), $ver = '1.0.0', $media = 'all');
-	wp_enqueue_style('timeline');
+        $shortcode_found = false;
+
+        foreach ($posts as $post) {
+
+            if ( !( stripos($post->post_content, '[wp-timeline') === false ) ) {
+                $shortcode_found = true;
+                break;
+            }
+        }
+
+        if ( $shortcode_found ) {
+            add_timeline_styles();
+        }
+        return $posts;
+    }
+
+add_action('the_posts', 'has_timeline_shortcode');
+
+//add styles to header
+function add_timeline_styles(){
+	add_action('wp_print_styles', 'timeline_styles');
+}
+function timeline_styles(){
+
+		wp_register_style($handle = 'timeline', $src = plugins_url('css/timeline.css', __FILE__), $deps = array(), $ver = '1.0.0', $media = 'all');
+		wp_enqueue_style('timeline');
 }
 
-add_action ('wp_enqueue_scripts', 'timeline_scripts');
-add_theme_support('post-thumbnails');
+
+//check featured image theme support, add
+function featured_image(){
+	if ( !current_theme_supports( 'post_thumbnails' ) ) {
+		if ( function_exists( 'add_theme_support' ) ) { 
+			
+			add_theme_support( 'post-thumbnails' );
+			add_image_size( 'timeline-thumb', 80, auto ); //300 pixels wide (and unlimited height)
+			return true;
+		}
+	}
+	else{
+		
+		add_image_size( 'timeline-thumb', 80, auto ); //300 pixels wide (and unlimited height)
+		return true;
+	}
+
+}
 add_filter('get_the_content', 'do_shortcode');
 add_filter('get_the_excerpt', 'do_shortcode');
 ?>
